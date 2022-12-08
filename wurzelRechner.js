57dfb5f63fd9582da4f3878d5342af3aaddf1ad4
LPDMinecraft <l_jost@online.de>
2022-12-08 17:44:33 +0100

    V2.1.2
    - Added estimated time

--- wurzelRechner.js
+++ wurzelRechner.js
@@ -1,88 +1,89 @@
 async function calc() {
     var n = 0, g = 0, r = 0, rr = 0, amount = (window.document.querySelector("[number]").value * 1), currentAmount = 0;
     var sq = "sq", log = true, system = "procent", numType = "N";
-    if(window.document.querySelector("[option-pro]").selected) system = "procent";
-    if(window.document.querySelector("[option-las]").selected) system = "last";
-    if(window.document.querySelector("[option-num]").selected) system = "number";
+    var maxFilePerSecound = 25000;
+    if (window.document.querySelector("[option-pro]").selected) system = "procent";
+    if (window.document.querySelector("[option-las]").selected) system = "last";
+    if (window.document.querySelector("[option-num]").selected) system = "number";
 
-    if(window.document.querySelector("[option-n]").selected) numType = "N";
-    if(window.document.querySelector("[option-g]").selected) numType = "G";
-    if(window.document.querySelector("[option-r]").selected) numType = "R";
-    if(window.document.querySelector("[option-rr]").selected) numType = "RR";
+    if (window.document.querySelector("[option-n]").selected) numType = "N";
+    if (window.document.querySelector("[option-g]").selected) numType = "G";
+    if (window.document.querySelector("[option-r]").selected) numType = "R";
+    if (window.document.querySelector("[option-rr]").selected) numType = "RR";
 
     log = window.document.querySelector("[debbug]").checked;
-    if(window.document.querySelector("[option-nro]").selected) sq = "nr";
-    if(window.document.querySelector("[option-sqr]").selected) sq = "sq";
+    if (window.document.querySelector("[option-nro]").selected) sq = "nr";
+    if (window.document.querySelector("[option-sqr]").selected) sq = "sq";
 
-    var waiter = 0; 
+    var waiter = 0;
     var stats = [];
     var start = (window.document.querySelector("[start]").value * 1);
     var fAmount = start + amount;
     stats.push(["Zahl", "Wert"]);
     var last = 100;
-    if(system == "last") last = 100;
-    if(system == "number") last = 0;
+    if (system == "last") last = 100;
+    if (system == "number") last = 0;
     for (let i = start; i <= fAmount; i++) {
         var current;
         if (sq === "nr") current = i;
         if (sq === "sq") current = Math.sqrt(i);
-        
+
         if (Number.isInteger(current) && i > 0) {
             if (log) console.log(i + " N - " + current);
-            if(numType === "N") {
-                if(system == "last") last = 100;
-                if(system == "number") last++;
+            if (numType === "N") {
+                if (system == "last") last = 100;
+                if (system == "number") last++;
             } else {
-                if(system == "last") last = last - 1;
+                if (system == "last") last = last - 1;
             }
             n++;
         } else if (Number.isInteger(current)) {
             if (log) console.log(i + " G - " + current);
-            if(numType == "G") {
-                if(system == "last") last = 100;
-                if(system == "number") last++;
+            if (numType == "G") {
+                if (system == "last") last = 100;
+                if (system == "number") last++;
             } else {
-                if(system == "last") last = last - 1;
+                if (system == "last") last = last - 1;
             }
             g++;
         } else if (!Number.isInteger(current)) {
             if (log) console.log(i + " R - " + current);
-            if(numType == "R") {
-                if(system == "last") last = 100;
-                if(system == "number") last++;
+            if (numType == "R") {
+                if (system == "last") last = 100;
+                if (system == "number") last++;
             } else {
-                if(system == "last") last = last - 1;
+                if (system == "last") last = last - 1;
             }
             r++;
         } else if (!Number.isInteger(current) && isFinite(current)) {
-            if(log) console.log(i + " RR - " + current);
-            if(numType == "RR") {
-                if(system == "last") last = 100;
-                if(system == "number") last++;
+            if (log) console.log(i + " RR - " + current);
+            if (numType == "RR") {
+                if (system == "last") last = 100;
+                if (system == "number") last++;
             } else {
-                if(system == "last") last = last - 1;
+                if (system == "last") last = last - 1;
             }
             rr++;
         }
         currentAmount++;
         waiter++;
-        if(system == "procent") {
-            if(numType == "N") {
+        if (system == "procent") {
+            if (numType == "N") {
                 stats.push([i, procent(n, currentAmount)]);
-            } else if(numType == "G") {
+            } else if (numType == "G") {
                 stats.push([i, procent(g, currentAmount)]);
-            } else if(numType == "R") {
+            } else if (numType == "R") {
                 stats.push([i, procent(r, currentAmount)]);
-            } else if(numType == "RR") {
+            } else if (numType == "RR") {
                 stats.push([i, procent(rr, currentAmount)]);
             }
         }
-        if(system == "last") stats.push([i, last]);
-        if(system == "number") stats.push([i, last]);
-        refresh(n, g, r, rr, currentAmount);
-        if(waiter > 25000) {
-            await timeout(200);
+        if (system == "last") stats.push([i, last]);
+        if (system == "number") stats.push([i, last]);
+        refresh(n, g, r, rr, currentAmount, maxFilePerSecound, fAmount);
+        if (waiter > maxFilePerSecound) {
             waiter = 0;
+            await timeout(600);
         }
     }
     console.log(stats);
@@ -94,17 +95,17 @@ async function calc() {
     link.setAttribute("href", encodedUri);
     var fileName = "flourish-" + start + "-" + amount + "-";
 
-    if(numType == "N") fileName = fileName + "N-";
-    else if(numType == "G") fileName = fileName + "G-";
-    else if(numType == "R") fileName = fileName + "R-";
-    else if(numType == "RR") fileName = fileName + "RR-";
+    if (numType == "N") fileName = fileName + "N-";
+    else if (numType == "G") fileName = fileName + "G-";
+    else if (numType == "R") fileName = fileName + "R-";
+    else if (numType == "RR") fileName = fileName + "RR-";
 
-    if(system == "procent") fileName = fileName + "procent-";
-    else if(system == "last") fileName = fileName + "last-";
-    else if(system == "number") fileName = fileName + "number-";
+    if (system == "procent") fileName = fileName + "procent-";
+    else if (system == "last") fileName = fileName + "last-";
+    else if (system == "number") fileName = fileName + "number-";
 
-    if(sq == "nr") fileName = fileName + "nr-";
-    else if(sq == "sq") fileName = fileName + "sq-";
+    if (sq == "nr") fileName = fileName + "nr-";
+    else if (sq == "sq") fileName = fileName + "sq-";
 
     fileName = fileName + version + ".csv";
 
@@ -146,8 +147,36 @@ function saveIntoTable(id, data) {
     table.appendChild(td2);
 }
 
-function refresh(n, g, r, rr, amount) {
+function estimatedTime(totalAmount, amount, maxFilePerSecound) {
+    var t = Math.round((totalAmount - amount) / maxFilePerSecound);
+    if (t < 0) t = 0;
+
+
+    var d = new Date(t * 1000);
+    console.log(t);
+    var hr = (d.getHours() - 1);
+    if (hr < 10) {
+        hr = "0" + hr;
+    }
+    var min = d.getMinutes();
+    if (min < 10) {
+        min = "0" + min;
+    }
+    var sec = d.getSeconds();
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    var ampm = "am";
+    if (hr > 12) {
+        hr -= 12;
+        ampm = "pm";
+    }
+    return hr + ":" + min + ":" + sec;
+}
+
+function refresh(n, g, r, rr, amount, maxFilePerSecound, totalAmount) {
     window.document.querySelector("[c-display]").textContent = "Anzahl:     " + amount;
+    window.document.querySelector("[c-time]").textContent = "Estimated time: " + estimatedTime(totalAmount, amount, maxFilePerSecound);
     window.document.querySelector("[n-amount-display]").textContent = n;
     window.document.querySelector("[g-amount-display]").textContent = g;
     window.document.querySelector("[r-amount-display]").textContent = r;
